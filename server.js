d0fb6d5618ee92f17caa181180dc98b56e51bce9
Michael-Hasan <abdukhodirovhasan2002@gmail.com>
2025-09-17 00:55:59 +0900

    harid.ejs fayli qoshildi va server.js yangilandi

--- server.js
+++ server.js
@@ -14,12 +14,17 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
-app.get("/hello", function(req, res) {
-    res.end(`<h1>Hello World</h1>`);
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success" });
+});
+
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function() {
-    console.log(`Server ${PORT} portda ishga tushdi`);
+server.listen(PORT, function () {
+  console.log(`Server ${PORT} portda ishga tushdi`);
 });
