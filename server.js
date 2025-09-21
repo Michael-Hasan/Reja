e8d56e0d00fb1a91fb89aaf9cc2f8d41a5454d43
Michael-Hasan <abdukhodirovhasan2002@gmail.com>
2025-09-21 22:26:30 +0900

    feat:publishing done! and event loop/callback va asynchronous functions

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 console.log("Web Serverni boshlash");
-const express = require('express');
+const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 const http = require("http");
@@ -7,16 +7,16 @@ const fs = require("fs");
 
 let user;
 fs.readFile("database/user.json", "utf-8", (err, data) => {
-    if (err) {
+  if (err) {
     console.log("ERROR", err);
-    } else {
-    user = JSON.parse(data)
-    }
-  });
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1: Kirish code
 app.use(express.static("public"));
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 2: Session code
@@ -25,11 +25,10 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
-app.post("/create-item", (req, res) => {
-});
+app.post("/create-item", (req, res) => {});
 
-app.get('/author', (req, res) => {
-  res.render('author', {user: user});
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
--- train.js
+++ train.js
@@ -1,30 +1,104 @@
-// TRAIN AREA
+// 21: NodeJS event loop va callback function organamiz //
 
 
-// TASK-1
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yaxshi talaba bo'ling", //0-20
+//     "togri boshliq tanlang va koproq xato qiling", //20-30
+//     "ozingizga ishlashni boshlang", //30-40
+//     "siz kuchli bolgan narsalarni qiling", //40-50
+//     "yoshlarga investitsiya qiling", //50-60
+//     "endi dam oling, foydasi yoq", //60
+// ];
 
-// console.log("Web Serverni boshlash");
-// const express = require('express');
-// const app = express();
-// const http = require("http");
+// function masahatBering(a, callback) {
+//     if(typeof a !== "number") callback("insert a number", null);
+//     else if(a <= 20) callback(null, list[0]);
+//     else if(a > 20 && a <= 30) callback(null, list[1]);
+//     else if(a > 30 && a <= 40) callback(null, list[2]);
+//     else if(a > 40 && a <= 50) callback(null, list[3]);
+//     else if(a > 50 && a <= 60) callback(null, list[4]);
+//     else {
+//         setTimeout(function() {
+//         callback(null, list[5]);
+//     }, 5000);
+//     }
+// }
 
-// // 1: Kirish code
-// app.use(express.static("public"));
-// app.use(express.json()); 
-// app.use(express.urlencoded({ extended: true }));
+// // masahatBering(10, (err, data) => {
+// //     if(err) console.log("ERROR:", err);
+// //     console.log('javob:', data);
+// // })
 
-// // 2: Session code
-// // 3: Views code
-// app.set("views", "views");
-// app.set("view engine", "ejs");
 
-// // 4: Routing code
-// app.get("/hello", function(req, res) {
-//     res.end(`<h1>Hello World</h1>`);
-// });
+// // masahatBering("salom", (err, data) => {
+// //     if(err) console.log("ERROR:", err);
+// //     else {
+// //         console.log('javob:', data);
+// //     }
+// // });
 
-// const server = http.createServer(app);
-// let PORT = 3000;
-// server.listen(PORT, function() {
-//     console.log(`Server ${PORT} portda ishga tushdi`);
-// });
+
+
+// console.log("passed here 0");
+// masahatBering(65, (err, data) => {
+//     if(err) console.log("ERROR:", err);
+//     else {
+//         console.log('javob:', data);
+//     }
+// })
+// console.log("passed here 1");
+
+//=================================================================================================================================//
+
+// 22: Asynchronous functionlarni organamiz //
+
+
+console.log("Jack Ma maslahatlari");
+const list = [
+    "yaxshi talaba bo'ling", //0-20
+    "togri boshliq tanlang va koproq xato qiling", //20-30
+    "ozingizga ishlashni boshlang", //30-40
+    "siz kuchli bolgan narsalarni qiling", //40-50
+    "yoshlarga investitsiya qiling", //50-60
+    "endi dam oling, foydasi yoq", //60
+];
+
+async function masahatBering(a) {
+    if(typeof a !== "number") throw new Error("insert a number", null);
+    else if(a <= 20) return list[0];
+    else if(a > 20 && a <= 30) return list[1];
+    else if(a > 30 && a <= 40) return list[2];
+    else if(a > 40 && a <= 50) return list[3];
+    else if(a > 50 && a <= 60) return list[4];
+    else {
+        return new Promise((resolve, reject) => {
+            setTimeout(function() {
+                resolve(list[5]);
+            }, 5000);
+        });
+    }
+}
+
+//the/catch usuli bilan chaqirish
+// console.log("passed here 0");
+// masahatBering(25)
+//   .then(data => {
+//     console.log('javob:', data);
+//   })
+//   .catch(err => {
+//     console.log("ERROR:", err);
+//   });
+// console.log("passed here 1");
+
+
+// async/await usuli bilan chaqirish
+async function run() {
+  let javob = await masahatBering(25);
+  console.log(javob);
+  javob = await masahatBering(70);
+  console.log(javob);
+  javob = await masahatBering(41);
+  console.log(javob);
+}
+run();
