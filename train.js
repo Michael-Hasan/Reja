a34676c653bd3dff9088b319af8b122cad5efd4e
Michael-Hasan <abdukhodirovhasan2002@gmail.com>
2025-09-22 16:59:55 +0900

    feat:MITASK Added

--- train.js
+++ train.js
@@ -1,6 +1,5 @@
 // 21: NodeJS event loop va callback function organamiz //
 
-
 // console.log("Jack Ma maslahatlari");
 // const list = [
 //     "yaxshi talaba bo'ling", //0-20
@@ -30,15 +29,12 @@
 // //     console.log('javob:', data);
 // // })
 
-
 // // masahatBering("salom", (err, data) => {
 // //     if(err) console.log("ERROR:", err);
 // //     else {
 // //         console.log('javob:', data);
 // //     }
 // // });
-
-
 
 // console.log("passed here 0");
 // masahatBering(65, (err, data) => {
@@ -53,32 +49,31 @@
 
 // 22: Asynchronous functionlarni organamiz //
 
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yaxshi talaba bo'ling", //0-20
+//   "togri boshliq tanlang va koproq xato qiling", //20-30
+//   "ozingizga ishlashni boshlang", //30-40
+//   "siz kuchli bolgan narsalarni qiling", //40-50
+//   "yoshlarga investitsiya qiling", //50-60
+//   "endi dam oling, foydasi yoq", //60
+// ];
 
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yaxshi talaba bo'ling", //0-20
-    "togri boshliq tanlang va koproq xato qiling", //20-30
-    "ozingizga ishlashni boshlang", //30-40
-    "siz kuchli bolgan narsalarni qiling", //40-50
-    "yoshlarga investitsiya qiling", //50-60
-    "endi dam oling, foydasi yoq", //60
-];
-
-async function masahatBering(a) {
-    if(typeof a !== "number") throw new Error("insert a number", null);
-    else if(a <= 20) return list[0];
-    else if(a > 20 && a <= 30) return list[1];
-    else if(a > 30 && a <= 40) return list[2];
-    else if(a > 40 && a <= 50) return list[3];
-    else if(a > 50 && a <= 60) return list[4];
-    else {
-        return new Promise((resolve, reject) => {
-            setTimeout(function() {
-                resolve(list[5]);
-            }, 5000);
-        });
-    }
-}
+// async function masahatBering(a) {
+//   if (typeof a !== "number") throw new Error("insert a number", null);
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 50) return list[3];
+//   else if (a > 50 && a <= 60) return list[4];
+//   else {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(function () {
+//         resolve(list[5]);
+//       }, 5000);
+//     });
+//   }
+// }
 
 //the/catch usuli bilan chaqirish
 // console.log("passed here 0");
@@ -91,14 +86,28 @@ async function masahatBering(a) {
 //   });
 // console.log("passed here 1");
 
-
 // async/await usuli bilan chaqirish
-async function run() {
-  let javob = await masahatBering(25);
-  console.log(javob);
-  javob = await masahatBering(70);
-  console.log(javob);
-  javob = await masahatBering(41);
-  console.log(javob);
+// async function run() {
+//   let javob = await masahatBering(25);
+//   console.log(javob);
+//   javob = await masahatBering(70);
+//   console.log(javob);
+//   javob = await masahatBering(41);
+//   console.log(javob);
+// }
+// run();
+
+//=================================================================================================================================//
+
+// MITASK - A;
+function countLetter(letter, word) {
+  let count = 0;
+  for (let char of word) {
+    if (char === letter) {
+      count++;
+    }
+  }
+  return count;
 }
-run();
+
+console.log(countLetter("e", "engineer"));
