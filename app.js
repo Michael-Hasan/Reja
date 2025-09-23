929b392ea2e0eb2ff63563c0c5b96431fcb3f644
Michael-Hasan <abdukhodirovhasan2002@gmail.com>
2025-09-23 19:13:33 +0900

    feat:CRUD operatsiyalari

--- app.js
+++ app.js
@@ -17,14 +17,36 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
-app.post("/create-item", (req, res) => {});
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  console.log("User entered /create-item");
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("Something went wrong");
+    } else {
+      res.end("successfully added");
+    }
+  });
+});
 
 app.get("/author", (req, res) => {
   res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
-  res.render("reja");
+  console.log("User entered /");
+  db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("something went wrong");
+      } else {
+        res.render("reja", { items: data });
+      }
+    });
 });
 
 module.exports = app;
