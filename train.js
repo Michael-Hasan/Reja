5c1680ceb54d3ca33054e16f3931a55399ce930f
Michael-Hasan <abdukhodirovhasan2002@gmail.com>
2025-10-03 19:19:45 +0900

    feat: E-Task added & Deploy done

--- train.js
+++ train.js
@@ -1,65 +1,75 @@
-// // MITASK - D;
+// MITASK - E;
 
-class Shop {
-  constructor(non, lagmon, cola) {
-    this.products = {
-      non,
-      lagmon,
-      cola,
-    };
-  }
-
-  _getTime() {
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, "0");
-    const minutes = String(now.getMinutes()).padStart(2, "0");
-    return `${hours}:${minutes}`;
-  }
-
-  qoldiq() {
-    const time = this._getTime();
-    const { non, lagmon, cola } = this.products;
-    console.log(
-      `Hozir ${time}da ${non}ta non, ${lagmon}ta lagmon va ${cola}ta cola mavjud!`
-    );
-  }
-
-  sotish(nomi, soni) {
-    if (this.products[nomi] === undefined) {
-      console.log(`Bunday mahsulot mavjud emas: ${nomi}`);
-      return;
-    }
-
-    if (this.products[nomi] < soni) {
-      console.log(
-        `Yetarli ${nomi} yo‘q. Faqat ${this.products[nomi]}ta mavjud.`
-      );
-      return;
-    }
-
-    this.products[nomi] -= soni;
-    const time = this._getTime();
-    console.log(`Hozir ${time}da ${soni}ta ${nomi} sotildi.`);
-  }
-
-  qabul(nomi, soni) {
-    if (this.products[nomi] === undefined) {
-      console.log(`Yangi mahsulot qabul qilinmaydi: ${nomi}`);
-      return;
-    }
-
-    this.products[nomi] += soni;
-    const time = this._getTime();
-    console.log(`Hozir ${time}da ${soni}ta ${nomi} qabul qilindi.`);
-  }
+function getReverse(str) {
+  return str.split("").reverse().join("");
 }
 
-const shop = new Shop(4, 5, 2);
+console.log(getReverse("hello"));
 
-shop.qoldiq();
-shop.sotish("non", 3);
-shop.qabul("cola", 4);
-shop.qoldiq();
+//=======================================================//
+
+// // // MITASK - D;
+
+// class Shop {
+//   constructor(non, lagmon, cola) {
+//     this.products = {
+//       non,
+//       lagmon,
+//       cola,
+//     };
+//   }
+
+//   _getTime() {
+//     const now = new Date();
+//     const hours = String(now.getHours()).padStart(2, "0");
+//     const minutes = String(now.getMinutes()).padStart(2, "0");
+//     return `${hours}:${minutes}`;
+//   }
+
+//   qoldiq() {
+//     const time = this._getTime();
+//     const { non, lagmon, cola } = this.products;
+//     console.log(
+//       `Hozir ${time}da ${non}ta non, ${lagmon}ta lagmon va ${cola}ta cola mavjud!`
+//     );
+//   }
+
+//   sotish(nomi, soni) {
+//     if (this.products[nomi] === undefined) {
+//       console.log(`Bunday mahsulot mavjud emas: ${nomi}`);
+//       return;
+//     }
+
+//     if (this.products[nomi] < soni) {
+//       console.log(
+//         `Yetarli ${nomi} yo‘q. Faqat ${this.products[nomi]}ta mavjud.`
+//       );
+//       return;
+//     }
+
+//     this.products[nomi] -= soni;
+//     const time = this._getTime();
+//     console.log(`Hozir ${time}da ${soni}ta ${nomi} sotildi.`);
+//   }
+
+//   qabul(nomi, soni) {
+//     if (this.products[nomi] === undefined) {
+//       console.log(`Yangi mahsulot qabul qilinmaydi: ${nomi}`);
+//       return;
+//     }
+
+//     this.products[nomi] += soni;
+//     const time = this._getTime();
+//     console.log(`Hozir ${time}da ${soni}ta ${nomi} qabul qilindi.`);
+//   }
+// }
+
+// const shop = new Shop(4, 5, 2);
+
+// shop.qoldiq();
+// shop.sotish("non", 3);
+// shop.qabul("cola", 4);
+// shop.qoldiq();
 
 //=======================================================//
 
